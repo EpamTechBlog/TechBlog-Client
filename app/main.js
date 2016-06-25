3cfcd21068cd9da7d78fbc9907fac5da2011a15e
XinlaiHe <xinlai_he@epam.com>
2016-06-25 18:06:26 +0800

    add auth check and redirect

--- app/main.js
+++ app/main.js
@@ -4,19 +4,31 @@ import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import SigninComponent from './src/components/signin.component.jsx';
 import SignupComponent from './src/components/signup.component.jsx';
 import NoMatchComponent from './src/components/signup.component.jsx';
-import HomeComponent from './src/components/home.component.jsx';
+import HomeComponent from './src/containers/home.component.jsx';
 import LandingComponent from './src/containers/landing.component.jsx';
 import App from './src/containers/app.component.jsx';
 import store from './store';
 import { Provider } from 'react-redux'
+import cookie from 'react-cookie';
 
 ReactDOM.render((
 	<Provider store={store}>
 	  <Router history={hashHistory}>
-	    <Route path='/' component={App}>
-	      <IndexRoute component={LandingComponent} />
-	      // add containers with path here...
+	    <Route component={App}>
+	      <Route path='/' onEnter={checkAuth} component={LandingComponent} />
 	    </Route>
+	    <Route path='/home' component={HomeComponent} />
 	  </Router>
 	</Provider>
 ), document.getElementById('content'))
+
+function checkAuth(nextState, replaceState) {
+  if (hasLogin()) {
+    replaceState({ nextPathname: nextState.location.pathname }, '/home');
+  }
+}
+
+function hasLogin(){
+
+	return (cookie.load('username') && cookie.load('userId'));
+}
