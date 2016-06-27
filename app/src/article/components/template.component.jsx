5130472ebb439af2eef71b82cebe1124f3ec07dd
CyrusCai <Cyrus_Cai@epam.com>
2016-06-27 16:08:51 +0800

    add folders for articles

---
+++ app/src/article/components/template.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+require('../../styles/home.style.css');
+class Template extends React.Component{
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+           )
+  }
+}
+
+export default Template;
