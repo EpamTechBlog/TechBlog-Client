7ca9e10cc6379c18eb5e9c96ec141753baf98b6e
Chris Yu <chris_yu@epam.com>
2016-06-28 14:28:28 +0800

    merge with uiframe2

--- app/src/core/containers/home.component.jsx
+++ app/src/core/containers/home.component.jsx
@@ -19,14 +19,8 @@ class HomeComponent extends React.Component{
 			<NavigationComponent />
 			<SidebarComponent />
 			</div>
-<<<<<<< HEAD:app/src/containers/home.component.jsx
 			)
 
-=======
-
-			)
-
->>>>>>> landingAndHome:app/src/core/containers/home.component.jsx
 	}
 }
 
