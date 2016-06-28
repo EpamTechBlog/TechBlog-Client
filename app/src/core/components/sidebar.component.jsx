2a4d47ccd9b9340ab974d1685dba9b7e2445d3b2
Irving Zhao <Irving_Zhao@epam.com>
2016-06-28 14:41:01 +0800

    adjust sidebar

--- app/src/core/components/sidebar.component.jsx
+++ app/src/core/components/sidebar.component.jsx
@@ -16,16 +16,16 @@ class SidebarComponent extends React.Component{
 			<div className="demo-drawer mdl-layout__drawer ">
 
 			<nav className="demo-navigation mdl-navigation ">
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">home</i>Home</a>
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">inbox</i>Inbox</a>
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">delete</i>Trash</a>
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">report</i>Spam</a>
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">forum</i>Forums</a>
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">flag</i>Updates</a>
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">local_offer</i>Promos</a>
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">shopping_cart</i>Purchases</a>
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">people</i>Social</a>
-			<a className="mdl-navigation__link" href=""><i className=" material-icons" role="presentation">help_outline</i>Help</a>
+			<a className="mdl-navigation__link addnew" href=""><i className="material-icons" role="presentation">add</i>New Posts</a>
+			<a className="mdl-navigation__link" href=""><i className="material-icons" role="presentation">home</i>Home</a>
+			<a className="mdl-navigation__link" href=""><i className="material-icons" role="presentation">inbox</i>Inbox</a>
+			<a className="mdl-navigation__link" href=""><i className="material-icons" role="presentation">delete</i>Trash</a>
+			<a className="mdl-navigation__link" href=""><i className="material-icons" role="presentation">report</i>Spam</a>
+			<a className="mdl-navigation__link" href=""><i className="material-icons" role="presentation">forum</i>Forums</a>
+			<a className="mdl-navigation__link" href=""><i className="material-icons" role="presentation">flag</i>Updates</a>
+			<a className="mdl-navigation__link" href=""><i className="material-icons" role="presentation">local_offer</i>Promos</a>
+			<a className="mdl-navigation__link" href=""><i className="material-icons" role="presentation">shopping_cart</i>Purchases</a>
+			<a className="mdl-navigation__link" href=""><i className="material-icons" role="presentation">help_outline</i>Help</a>
 			</nav>
 			</div>
 			)
