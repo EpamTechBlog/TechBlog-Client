b215d0b788ae3bf1ed9cf732f00c8b60f6c3ac26
peidong yuan <yuanpeidong1991@gmail.com>
2016-06-29 11:32:44 +0800

    fixed bar

---
+++ app/editorBox.js
@@ -0,0 +1,13 @@
+tinymce.init({
+	selector: '#myTextarea',
+	theme: 'modern',
+	width: 600,
+	height: 300,
+	plugins: [
+	'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+	'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+	'save table contextmenu directionality emoticons template paste textcolor'
+	],
+	content_css: 'css/content.css',
+	toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
+});
---
+++ app/src/core/components/comment.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link, hashHistory } from 'react-router';
+import cookie from 'react-cookie';
+import $ from "jquery";
+require('../../../styles/comment.style.css');
+class CommentComponent extends React.Component{
+
+	constructor(){
+
+		super();
+		this.state = { username : cookie.load('username'), userId : cookie.load('userId') };
+
+	}
+
+	
+
+	render(){
+
+		return (
+			<div className="commentEditor">	
+			<textarea></textarea>
+
+			</div>
+			)
+	}
+
+	logout() {
+
+		cookie.remove('username', { path: '/' });
+		cookie.remove('userId', { path: '/' });
+		this.setState({username : undefined, userId : undefined});
+
+	}
+}
+export default CommentComponent;
